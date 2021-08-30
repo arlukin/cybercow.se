8b3ac040375ce139e2a97eb3113d4fbe112ff434
Arlukin <daniel@cybercow.se>
2021-08-30 19:37:44 +0200

    fix: new linkedin name

--- config.js
+++ config.js
@@ -1,32 +1,33 @@
 module.exports = {
-  siteTitle: 'Cybercow',
-  manifestName: 'Aerial',
-  manifestShortName: 'Portfolio', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/sacred-cow.svg',
+  siteTitle: "Cybercow",
+  manifestName: "Aerial",
+  manifestShortName: "Portfolio", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: "#663399",
+  manifestThemeColor: "#663399",
+  manifestDisplay: "standalone",
+  manifestIcon: "src/assets/img/sacred-cow.svg",
   pathPrefix: `/`, // This path is subpath of your hosting https://domain/portfolio
   // social
 
-  authorName: 'Daniel Lindh',
-  authorHeading: 'Unicorn Breeder - Business Developer - System Architect - System Developer - System Operator',
+  authorName: "Daniel Lindh",
+  authorHeading:
+    "Unicorn Breeder - Business Developer - System Architect - System Developer - System Operator",
   authorSocialLinks: [
     {
-      icon: 'fa-linkedin',
-      name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/therealdaniellindh',
+      icon: "fa-linkedin",
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/daniel-lindh-entrepreneur"
     },
     {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/arlukin',
+      icon: "fa-github",
+      name: "Github",
+      url: "https://github.com/arlukin"
     },
     {
-      icon: 'fa-envelope-o',
-      name: 'Email',
-      url: 'mailto:my-first-name@cybercow.se',
-    },
-  ],
+      icon: "fa-envelope-o",
+      name: "Email",
+      url: "mailto:my-first-name@cybercow.se"
+    }
+  ]
 };
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
-const config = require('./config');
+const config = require("./config");
 
 module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
-    title: config.siteTitle,
+    title: config.siteTitle
   },
   plugins: [
     {
@@ -32,10 +32,10 @@ module.exports = {
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
-      },
+        cookieDomain: "example.com"
+      }
     },
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -45,10 +45,10 @@ module.exports = {
         background_color: config.manifestBackgroundColor,
         theme_color: config.manifestThemeColor,
         display: config.manifestDisplay,
-        icon: config.manifestIcon, // This path is relative to the root of the site.
-      },
+        icon: config.manifestIcon // This path is relative to the root of the site.
+      }
     },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
-  ],
+    "gatsby-plugin-sass",
+    "gatsby-plugin-offline"
+  ]
 };
